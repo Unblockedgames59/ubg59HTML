73c96cec3c8d435b8d88454471b6ba3773efd424
59GuyDev <128239680+MongrelTarsier3@users.noreply.github.com>
2023-06-15 20:19:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ input.addEventListener('input', () => {
     }
   });
 });
+
+// Code to open game links in a new tab and create an iframe
+const gameLinks = document.querySelectorAll('#games a');
+
+gameLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const gameUrl = link.getAttribute('href');
+
+    // Open the game link in a new tab
+    const newTab = window.open('about:blank', '_blank');
+    newTab.document.write(`<iframe src="${gameUrl}" width="100%" height="100%"></iframe>`);
+  });
+});
