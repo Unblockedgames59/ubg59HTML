193489f75be34d1409d4d8cebba578dd586193ea
59GuyDev <128239680+MongrelTarsier3@users.noreply.github.com>
2023-06-16 07:49:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const input = document.querySelector('input');
 const games = document.querySelectorAll('#games img');
+const titles = document.querySelectorAll('#games a span');
 
 input.addEventListener('input', () => {
   const searchTerm = input.value.toLowerCase();
-  games.forEach(game => {
+  games.forEach((game, index) => {
+    const title = titles[index];
     if (game.alt.toLowerCase().includes(searchTerm)) {
-      game.style.display = 'block';
+      game.parentNode.style.display = 'block';
     } else {
-      game.style.display = 'none';
+      game.parentNode.style.display = 'none';
     }
   });
 });
